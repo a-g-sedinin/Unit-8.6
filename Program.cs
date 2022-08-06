e299584d1c948370d7866507db504cb7e57faf2c
a.g.sedinin <s2k-r59@mail.ru>
2022-08-06 14:02:08 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unit_8._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
