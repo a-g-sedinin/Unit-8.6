c7b1c3ad3c94b131df9647bfcdd27d52b7baa582
a.g.sedinin <s2k-r59@mail.ru>
2022-08-09 00:03:24 +0500

    Task 4 completed

--- Program.cs
+++ Program.cs
@@ -196,11 +196,37 @@ namespace FinalTask
             Console.Clear();
             Console.WriteLine("Task #4 has been choosen.");
             BinaryFormatter formatter = new BinaryFormatter();
-            using FileStream fs = new FileStream("D://Students.dat", FileMode.OpenOrCreate);
+            using FileStream fs = new FileStream(GetFile(), FileMode.OpenOrCreate);
             {
                 try
                 {
-                    var st = formatter.Deserialize(fs);
+                    Student[] st = (Student[]) formatter.Deserialize(fs);
+                    string destopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                    DirectoryInfo dirinfo = new DirectoryInfo(destopPath + "/Students/");
+                    if (!dirinfo.Exists) dirinfo.Create();
+                    
+                    string dirFullName = dirinfo.FullName;
+                    
+                    foreach (Student stud in st)
+                    {
+                        string filePath = dirFullName + stud.Group + ".txt";
+                        string strToWrite = stud.Name + ", " + stud.DateOfBirth.ToString();
+                        if (!File.Exists(filePath))
+                        {
+                            using (StreamWriter sw = File.CreateText(filePath));
+                        }
+                        else
+                        {
+                            FileInfo fileInfo = new FileInfo(filePath);
+                            using (StreamWriter sw = fileInfo.AppendText())
+                            {
+                                sw.WriteLine(strToWrite);
+                            }
+                        
+                        }
+                        
+                    }
+                    Console.WriteLine("Sorting complete.");
                     Console.ReadKey();
                 }
                 catch (Exception e)
